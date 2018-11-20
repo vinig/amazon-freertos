a6772cc7a979dcd16c90d9df0a0216c9ef364f5a
Vini Gupta <vinigupt@amazon.com>
2018-11-19 17:01:55 -0800

    Attempting to blink LEDs.

--- demos/espressif/esp32_devkitc_esp_wrover_kit/common/application_code/main.c
+++ demos/espressif/esp32_devkitc_esp_wrover_kit/common/application_code/main.c
@@ -43,6 +43,7 @@
 #include "esp_system.h"
 #include "esp_wifi.h"
 #include "esp_interface.h"
+#include "driver/gpio.h"
 
 /* Application version info. */
 #include "aws_application_version.h"
@@ -51,6 +52,7 @@
 #define mainLOGGING_MESSAGE_QUEUE_LENGTH    ( 32 )
 #define mainLOGGING_TASK_STACK_SIZE         ( configMINIMAL_STACK_SIZE * 6 )
 #define mainDEVICE_NICK_NAME                "Espressif_Demo"
+#define BLINK_GPIO CONFIG_BLINK_GPIO
 
 /* Declare the firmware version structure for all to see. */
 const AppVersion32_t xAppFirmwareVersion = {
@@ -129,6 +131,29 @@ static void prvWifiConnect( void );
 static void prvMiscInitialization( void );
 /*-----------------------------------------------------------*/
 
+/* MY DEMO CODE */
+void blink_task(void *pvParameter)
+{
+    /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
+       muxed to GPIO on reset already, but some default to other
+       functions and need to be switched to GPIO. Consult the
+       Technical Reference for a list of pads and their default
+       functions.)
+    */
+    gpio_pad_select_gpio(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    while(1) {
+        /* Blink off (output low) */
+        gpio_set_level(BLINK_GPIO, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        /* Blink on (output high) */
+        gpio_set_level(BLINK_GPIO, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+
 /**
  * @brief Application runtime entry point.
  */
@@ -149,16 +174,7 @@ int app_main( void )
 
     if( SYSTEM_Init() == pdPASS )
     {
-        /* Connect to the wifi before running the demos */
-        prvWifiConnect();
-
-        /* A simple example to demonstrate key and certificate provisioning in
-        * microcontroller flash using PKCS#11 interface. This should be replaced
-        * by production ready key provisioning mechanism. */
-        vDevModeKeyProvisioning();
-
-        /* Run all demos. */
-        DEMO_RUNNER_RunDemos();
+        xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
     }
 
     /* Start the scheduler.  Initialization that requires the OS to be running,
